59b1fab09f0bd725004a348f581d8f543568cb19
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-04-06 01:25:08 +0300

    fix bug

--- src/shared/ui/icon/Icon.vue
+++ src/shared/ui/icon/Icon.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import { defineAsyncComponent, defineOptions } from 'vue'
+import { defineAsyncComponent } from 'vue'
 
 interface Props {
   name: string
