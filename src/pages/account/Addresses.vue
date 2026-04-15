b2c281a9d1d46a5c0fe70053179ab0010b33fb04
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-04-15 17:54:08 +0300

    create address page layout

--- src/pages/account/Addresses.vue
+++ src/pages/account/Addresses.vue
@@ -39,32 +39,87 @@ function onSubmit() {
 </script>
 
 <template>
-  <form class="account-addresses" aria-label="Addresses content" @submit.prevent="onSubmit">
-    <h2 class="account-addresses__title">Billing address</h2>
-
-    <BillingDetailsForm
-      class="account-addresses__form"
-      :country-options="countryOptions"
-      :show-checkboxes="false"
-      :show-order-notes="false"
-      :show-submit="true"
-      submit-label="SAVE ADDRESS"
-    />
-  </form>
+  <div class="address-page">
+    <div class="app-container">
+      <div class="address-page__description">
+        <p class="address-page__description-text">We’ve received your order</p>
+      </div>
+      <div class="address-page__body">
+        <div class="address-page__billing-details">
+          <form
+            class="address-page__form"
+            aria-label="Addresses content"
+            @submit.prevent="onSubmit"
+          >
+            <h2 class="address-page__billing-details-title h2-title">Order Details</h2>
+            <BillingDetailsForm
+              class="address-page__form"
+              :country-options="countryOptions"
+              :show-checkboxes="false"
+              :show-order-notes="false"
+              :show-submit="true"
+              submit-label="SAVE ADDRESS"
+            />
+          </form>
+        </div>
+        <div class="address-page__order">
+          <h2 class="address-page__order-title h2-title">Order Summary</h2>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.account-addresses {
-  &__title {
-    margin: 0 0 globalFunctions.fluidValue(18px, 28px, 320px, 1440px);
+.address-page {
+  $breakpoint-md: 1024px;
+
+  font-family: var(--font-family);
+
+  .h2-title {
     font-family: var(--font-family);
     font-weight: 400;
     font-size: globalFunctions.fluidValue(16px, 26px, 320px, 1440px);
     color: var(--light-colors-black---light);
+    margin-bottom: globalFunctions.fluidValue(21px, 26px, 320px, 1440px);
+  }
+
+  &__title {
+    font-family: var(--font-family);
+    font-weight: 500;
+    font-size: globalFunctions.fluidValue(16px, 31px, 320px, 1440px);
+    color: var(--light-colors-black---light);
+    text-align: center;
+    margin-bottom: globalFunctions.fluidValue(15px, 65px, 320px, 1440px);
+
+    @media (max-width: globalBreakpoints.$breakpoint-sm) {
+      font-weight: 400;
+      text-align: left;
+    }
+  }
+
+  &__body {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: globalFunctions.fluidValue(39px, 90px, 320px, 1440px);
+
+    @media (max-width: $breakpoint-md) {
+      grid-template-columns: 1fr;
+    }
   }
 
   &__form {
-    max-width: 680px;
+    &__title {
+      margin: 0 0 globalFunctions.fluidValue(18px, 28px, 320px, 1440px);
+      font-family: var(--font-family);
+      font-weight: 400;
+      font-size: globalFunctions.fluidValue(16px, 26px, 320px, 1440px);
+      color: var(--light-colors-black---light);
+    }
+
+    &__form {
+      max-width: 680px;
+    }
   }
 }
 </style>
